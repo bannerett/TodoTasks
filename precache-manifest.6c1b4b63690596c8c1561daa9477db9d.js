41473e25fe443456abf144a164336500a742fa80
johnv87 <johnv87@users.noreply.github.com>
2020-06-07 00:19:21 +0200

    Updates

---
+++ precache-manifest.6c1b4b63690596c8c1561daa9477db9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184b419aca78c0cb6892bc08a49ba576",
+    "url": "/TodoTasks/index.html"
+  },
+  {
+    "revision": "683c9efa2ef12e34f664",
+    "url": "/TodoTasks/static/css/main.40fa284c.chunk.css"
+  },
+  {
+    "revision": "484536a0177f8c796abc",
+    "url": "/TodoTasks/static/js/2.9c9b853a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoTasks/static/js/2.9c9b853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683c9efa2ef12e34f664",
+    "url": "/TodoTasks/static/js/main.02d2b20c.chunk.js"
+  },
+  {
+    "revision": "586de7de87fb3efedeca",
+    "url": "/TodoTasks/static/js/runtime-main.d25275be.js"
+  }
+]);
